84d0b8b98476e58e110bf8d7f865a537502efa71
dayongkr <dayongkr@gmail.com>
2023-08-26 15:19:52 +0900

    fix: change logo link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Logo from "@/public/3d_Logo.png";
+import Logo from "@/public/3d_logo.png";
 import Title from "@/components/Title";
 import Paragraph from "@/components/Paragraph";
 import Button from "@/components/Button";
